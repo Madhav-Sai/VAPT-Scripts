2333699a0708ef3490fabee9a8a057e1dd3edd41
MADHAV <86304571+Madhav-Sai@users.noreply.github.com>
2024-08-09 15:53:26 +0530

    Update home_dir_access_check.sh
    added code

--- home_dir_access_check.sh
+++ home_dir_access_check.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-if [[ ! -r "/etc/passwd" ]]; then 
+if [[ ! -r "/etc/passwd" ]]; then
+    echo "/etc/passwd must exist and be readable to continue." 
+    exit 1 
+fi 
+
+while read -r line; do 
+    account=$(echo "${line}" | awk -F':' '{print $1}')
+    home_dir=$(echo "${line}" | awk -F':' '{print $}')
+    
+    # Target only home directories under /home.
+    if echo "${home_dir}" | grep -1 "^/home"; then
+        echo " Home directory ${home_dir} of ${account} is accesssiable!"
+    else
+        echo "Home directory ${home_dir} of ${account} is Not accessible!"
+    fi
+fi
+done < < (cat "/etc/passwd")
+
+
+
